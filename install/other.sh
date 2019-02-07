dda7ccc4e972c12076d8cdd6a98089011dbccb8e
vikin91 <vikin91@users.noreply.github.com>
2019-02-07 11:07:40 +0100

    Harden Powerline font installation

--- install/other.sh
+++ install/other.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 # Powerline fonts https://github.com/powerline/fonts
-git clone https://github.com/powerline/fonts.git --depth=1 ~/fonts
-cd "${HOME}"/fonts || exit 1
+TMP_FONT_DIR="/tmp/fonts"
+mkdir -p "${TMP_FONT_DIR}"
+git clone https://github.com/powerline/fonts.git --depth=1 "${TMP_FONT_DIR}"
+cd "${TMP_FONT_DIR}" || exit 1
 ./install.sh
-cd "${HOME}" || exit 1
-rm -rf ~/fonts
+cd "${TMP_FONT_DIR}" || exit 1
+rm -rf "${TMP_FONT_DIR}"
 
